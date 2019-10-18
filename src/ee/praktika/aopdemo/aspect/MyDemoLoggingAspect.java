7e5ae5e75e9e94593ad5126e6162fd0e90bbbd03
AntsErik <ants.erik.noormagi@aweroc.com>
2019-10-18 15:46:12 +0300

    Added Pointcut annotation. Added another Before-Advice to reuse the Pointcut declaration.

--- src/ee/praktika/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/ee/praktika/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,6 +2,7 @@ package ee.praktika.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
@@ -12,8 +13,18 @@ public class MyDemoLoggingAspect {
 
     //let us start with a before advice
 
-    @Before( "execution(* ee.praktika.aopdemo.dao.*.*(..))" )
+    @Pointcut( "execution(* ee.praktika.aopdemo.dao.*.*(..))" )
+    private void referencePointcut(){
+
+    }
+
+    @Before( "referencePointcut()" )
     public void beforeAddAccountAdvice(){
         System.out.println( "\n======>>> Executing @Before advice on addAccount() in the DAO package" );
+    }
+
+    @Before( "referencePointcut()" )
+    public void performApiAnalytics(){
+        System.out.println( "\n======>>> Doing some cool analytics down here." );
     }
 }
